34a1090f8981ca617143ace5babcc13d0446786d
Thomas Schmidt <thomas.michel.schmidt@gmail.com>
2026-06-16 16:09:53 +0200

    pimping css

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ module.exports = {
     "./*.js",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+        display: ['Space Grotesk', 'sans-serif'],
+      }
+    },
   },
   plugins: [require("@tailwindcss/typography")],
 };
